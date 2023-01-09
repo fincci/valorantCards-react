18d2df15208e4e8826bf17363d690a54f1e5f69a
Filipe Carvalho <lp-oliveirac@hotmail.com>
2023-01-09 14:31:32 -0300

    sound add

--- src/components/agentList/agentList.js
+++ src/components/agentList/agentList.js
@@ -1,12 +1,39 @@
+import { Howl } from 'howler'
+import hoverSound from '../../assets/sounds/hoverSound.mp3'
+import clickSound from '../../assets/sounds/clickSound.mp3'
 import "./agentList.css"
 
 const AgentList = ({ clickEvent, props }) => {
+
+    const hover = new Howl({
+        src: [hoverSound],
+        html5: true,
+        preload: true,
+        volume: 1
+    })
+
+    const click = new Howl({
+        src: [clickSound],
+        html5: true,
+        preload: true,
+        volume: 1
+    })
+
     return (
         <ul className="agent-list">
             {props
                 .filter(data => data.isPlayableCharacter)
                 .map((agent, index) =>
-                    <li onClick={() => clickEvent(agent.displayName)} className="agent" name={agent.displayName} key={index}>
+                    <li
+                        onMouseEnter={() => hover.play()}
+                        onClick={() => {
+                            clickEvent(agent.displayName);
+                            click.play();
+                        }}
+                        className="agent"
+                        name={agent.displayName}
+                        key={index}
+                    >
                         <img src={agent.displayIconSmall} alt={`Personagem ${agent.displayName}`} />
                     </li>
                 )}
